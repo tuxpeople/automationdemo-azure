a8673c2ab44a616eb8fe5668604cf8a7bf9164a4
Thomas Deutsch <thomas@tuxpeople.org>
2021-05-05 16:48:46 +0200

    format fixes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,19 @@
 variable "prefix" {
- type = "string"
- default = "myprefix"
+  type = "string"
+  default = "myprefix"
 }
 
 variable "resource_group_name" {
- type = "string"
- default = "myrg"
+  type = "string"
+  default = "myrg"
 }
 
 variable "location" {
- type = "string"
- default = "eastus"
+  type = "string"
+  default = "eastus"
 }
 
 variable "domainname" {
- type = "string"
- default = "mydomain"
+  type = "string"
+  default = "mydomain"
 }
