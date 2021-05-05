1999f2263f8c2ae9a7c9542b2fc2f36a791283f2
Thomas Deutsch <thomas@tuxpeople.org>
2021-05-05 15:33:14 +0200

    replace provider block

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,13 +5,14 @@ terraform {
     container_name       = "tfstatedevops"
     key                  = "terraformgithubexample.tfstate"
   }
-}
 
-provider "azurerm" {
-  # The "feature" block is required for AzureRM provider 2.x.
-  # If you're using version 1.x, the "features" block is not allowed.
-  version = "~>2.0"
-  features {}
+  required_providers {
+    mycloud = {
+      source  = "azurerm"
+      version = "~>2.0"
+      features {}
+    }
+  }
 }
 
 data "azurerm_client_config" "current" {}
