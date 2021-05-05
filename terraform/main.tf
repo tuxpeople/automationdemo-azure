3c6e39d6480ddf479d5468835fa1ee39e0ebc4eb
Thomas Deutsch <thomas@tuxpeople.org>
2021-05-05 19:21:55 +0200

    split

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,20 +51,6 @@ resource "azurerm_public_ip" "pip" {
   reverse_fqdn        = "${var.domainname}.${var.location}.cloudapp.azure.com."
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "${var.vm_name}-nic-${count.index}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  tags                = var.tags
-  count               = "3"
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
 resource "azurerm_network_security_group" "k3sserver" {
   name                = "ingress_k3sserver"
   location            = azurerm_resource_group.rg.location
@@ -93,55 +79,8 @@ resource "azurerm_network_security_group" "k3sserver" {
   }
 }
 
-resource "azurerm_network_interface_security_group_association" "main" {
-  network_interface_id      = azurerm_network_interface.internal.id
-  network_security_group_id = azurerm_network_security_group.k3sserver.id
-}
-
 data "template_file" "master-cloud-init" {
   template = file("../scripts/master-cloud-init.txt")
-}
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                  = "${var.vm_name}-vm-1"
-  resource_group_name   = azurerm_resource_group.rg.name
-  location              = azurerm_resource_group.rg.location
-  size                  = "Standard_B2s"
-  network_interface_ids = azurerm_network_interface.nic.1.id
-  admin_username        = "adminuser"
-  custom_data           = base64encode(data.template_file.master-cloud-init.rendered)
-
-  os_disk {
-    name                 = "osdisk-1"
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-
-  resource "azurerm_managed_disk" "managed_disk" {
-    name                 = datadisk-1
-    location             = azurerm_resource_group.rg.location
-    resource_group_name  = azurerm_resource_group.rg.name
-    storage_account_type = "Standard_LRS"
-    create_option        = "Empty"
-    disk_size_gb         = 10
-    tags                 = var.tags
-  }
-
-  resource "azurerm_virtual_machine_data_disk_attachment" "managed_disk_attach" {
-    managed_disk_id    = azurerm_managed_disk.managed_disk.1.id
-    virtual_machine_id = azurerm_linux_virtual_machine.vm.1.id
-    lun                = 10
-    caching            = "ReadWrite"
-  }
-
-  tags = var.tags
 }
 
 output "fqdn" {
