eaea5cd0ddd292cb8617e841045c7c2a74e920dd
Thomas Deutsch <thomas@tuxpeople.org>
2021-05-05 11:11:49 +0200

    fix ip range

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,8 +23,8 @@ resource "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_virtual_network" "main" {
-  name                = "tedops-network"
-  address_space       = ["10.0.3.0/16"]
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
@@ -33,7 +33,7 @@ resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.5.0/24"]
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_public_ip" "pip" {
