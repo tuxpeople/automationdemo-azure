e3cc33f2b55779663c4b38260347332d89cedd0d
Thomas Deutsch <thomas@tuxpeople.org>
2021-05-05 11:13:51 +0200

    replace prefix

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
+  name                = "tedops-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
