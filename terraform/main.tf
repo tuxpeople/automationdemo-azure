048cca4fa424661431ebbd8ef0958ec2f7f25a61
Thomas Deutsch <thomas@tuxpeople.org>
2021-05-05 19:31:07 +0200

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,38 +23,63 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 #Create Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+resource "azurerm_resource_group" "main" {
+  name     = "tedops"
+  location = "eastus2"
 }
 
 resource "azurerm_virtual_network" "main" {
-  name                = "${var.vnet_name}-network"
+  name                = "tedops-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 }
 
 resource "azurerm_subnet" "internal" {
-  name                 = var.vnet_name
-  resource_group_name  = azurerm_resource_group.rg.name
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_public_ip" "pip" {
-  name                = "${var.prefix}-pip"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  name                = "tedops-pip"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
   allocation_method   = "Dynamic"
-  domain_name_label   = var.domainname
-  reverse_fqdn        = "${var.domainname}.${var.location}.cloudapp.azure.com."
+  domain_name_label   = "automationdemo-tedops"
+  reverse_fqdn        = "automationdemo-tedops.eastus2.cloudapp.azure.com."
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "tedops-nic1"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "primary"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_network_interface" "internal" {
+  name                = "tedops-nic2"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
 resource "azurerm_network_security_group" "k3sserver" {
   name                = "ingress_k3sserver"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
   security_rule {
     access                     = "Allow"
     direction                  = "Inbound"
@@ -79,8 +104,68 @@ resource "azurerm_network_security_group" "k3sserver" {
   }
 }
 
+resource "azurerm_network_interface_security_group_association" "main" {
+  network_interface_id      = azurerm_network_interface.internal.id
+  network_security_group_id = azurerm_network_security_group.k3sserver.id
+}
+
+resource "azurerm_managed_disk" "master" {
+  name                 = tedops-data
+  resource_group_name  = azurerm_resource_group.main.name
+  location             = azurerm_resource_group.main.location
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 10
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "master" {
+  managed_disk_id    = azurerm_managed_disk.master.id
+  virtual_machine_id = azurerm_linux_virtual_machine.main.id
+  lun                = "10"
+  caching            = "ReadWrite"
+}
+
 data "template_file" "master-cloud-init" {
   template = file("../scripts/master-cloud-init.txt")
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                = "tedops-vm"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_B2s"
+  admin_username      = "adminuser"
+  custom_data         = base64encode(data.template_file.master-cloud-init.rendered)
+
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+    azurerm_network_interface.internal.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("/github/workspace/id_rsa.pub")
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
+
+output "public_ip" {
+  value = azurerm_linux_virtual_machine.main.public_ip_address
+}
+
+output "utility_ip" {
+  value = azurerm_linux_virtual_machine.main.private_ip_address
 }
 
 output "fqdn" {
