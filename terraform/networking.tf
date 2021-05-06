a3d36322a8ef89aa0068d79d762bd62c91ef7fb5
Thomas Deutsch <thomas@tuxpeople.org>
2021-05-06 14:52:22 +0200

    try to fix

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -89,7 +89,7 @@ resource "azurerm_network_interface" "main" {
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "association" {
-  network_interface_id    = "azurerm_network_interface.main*.id"
+  network_interface_id    = "azurerm_network_interface.main[${count.index}].id"
   ip_configuration_name   = "testconfiguration${count.index}"
   backend_address_pool_id = azurerm_lb_backend_address_pool.backend.id
   count                   = "3"
