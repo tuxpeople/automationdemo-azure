660bea7636960b4fa509e42a053ee3828d44b857
Thomas Deutsch <thomas@tuxpeople.org>
2021-05-06 20:16:58 +0200

    it is tuple with 3 elements

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,13 +1,5 @@
-output "server1" {
-  value = azurerm_linux_virtual_machine.k3s.1.private_ip_address
-}
-
-output "server2" {
-  value = azurerm_linux_virtual_machine.k3s.2.private_ip_address
-}
-
-output "server3" {
-  value = azurerm_linux_virtual_machine.k3s.3.private_ip_address
+output "k3sservers" {
+  value = ["${azurerm_linux_virtual_machine.k3s.*.private_ip_address}"]
 }
 
 output "web" {
