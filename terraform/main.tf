3fdcee8ff4bb5848e540d3aec19dbabbb5e377cb
Thomas Deutsch <thomas@tuxpeople.org>
2021-05-06 09:42:04 +0200

    "address_prefix": [DEPRECATED] Use the `address_prefixes` property instead.

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,14 +38,14 @@ resource "azurerm_subnet" "subnet1" {
   name                 = "subnet1"
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
-  address_prefix       = "10.0.0.0/24"
+  address_prefixes     = ["10.0.0.0/24"]
 }
 
 resource "azurerm_subnet" "subnet2" {
   name                 = "subnet2"
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
-  address_prefix       = "10.0.1.0/24"
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "main" {
