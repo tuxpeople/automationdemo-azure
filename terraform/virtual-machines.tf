377736aafaaa4d2536c49f398c16322e785c33a1
Thomas Deutsch <thomas@tuxpeople.org>
2021-05-06 12:50:16 +0200

    trying to fix folder references

--- terraform/virtual-machines.tf
+++ terraform/virtual-machines.tf
@@ -19,7 +19,7 @@ resource "azurerm_linux_virtual_machine" "test" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("/github/workspace/id_rsa.pub")
+    public_key = file("id_rsa.pub")
   }
 
   source_image_reference {
