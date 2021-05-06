be683e6bd637aab0cf0301cc2bbba193767a3b0c
Thomas Deutsch <thomas@tuxpeople.org>
2021-05-06 10:59:10 +0200

    added azurerm_availability_set

--- terraform/virtual-machines.tf
+++ terraform/virtual-machines.tf
@@ -1,8 +1,18 @@
+resource "azurerm_availability_set" "avset" {
+  name                         = "avset"
+  location                     = azurerm_resource_group.rg.location
+  resource_group_name          = azurerm_resource_group.rg.name
+  platform_fault_domain_count  = 3
+  platform_update_domain_count = 3
+  managed                      = true
+}
+
 resource "azurerm_linux_virtual_machine" "test" {
   name                  = "${var.vm_name}-vm-${count.index}"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = ["${element(azurerm_network_interface.main.*.id, count.index)}"] #["${azurerm_network_interface.main.id}"]
+  availability_set_id   = azurerm_availability_set.avset.id
   size                  = "Standard_B2s"
   count                 = "3"
   admin_username        = "adminuser"
