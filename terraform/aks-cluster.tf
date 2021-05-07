8a20a1e744e22f439673540802d984b1df80af33
Thomas Deutsch <thomas@tuxpeople.org>
2021-05-07 14:36:27 +0200

    fix kubeconfig location

--- terraform/aks-cluster.tf
+++ terraform/aks-cluster.tf
@@ -32,5 +32,5 @@ resource "azurerm_kubernetes_cluster" "default" {
 
 resource "local_file" "kube_config" {
   content  = azurerm_kubernetes_cluster.default.kube_config_raw
-  filename = "../.kube/config/kube_config"
+  filename = "../.kube/config"
 }
